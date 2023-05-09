e80f3960e1936fe2742e4cedace5b4be7f885b62
crackhopper <crackhopper@qq.com>
2023-05-09 13:02:53 +0800

    默认用Ascend卡

--- run-gpt2-tiny.sh
+++ run-gpt2-tiny.sh
@@ -1,3 +1,3 @@
 python run_mindformer.py --config configs/gpt2/run_gpt2-tiny.yaml \
                          --run_mode train \
-                         --device_target CPU 
+                         --device_target Ascend 
