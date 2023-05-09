9813522e1c691e0a64d27894b2807b192b75716a
crackhopper <crackhopper@gmail.com>
2023-05-09 14:14:55 +0800

    单节点上的测试配置完善

---
+++ run-8cards-huge.sh
@@ -0,0 +1,5 @@
+./scripts/run_distribute.sh \
+    /user/config/nbstart_hccl.json \
+    configs/gpt2/run_gpt2-large.yaml \
+    [0,8] \
+    train
---
+++ run-gpt2-medium.sh
@@ -0,0 +1,3 @@
+python run_mindformer.py --config configs/gpt2/run_gpt2-medium.yaml \
+                         --run_mode train \
+                         --device_target Ascend 
